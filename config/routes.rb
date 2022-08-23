e126f1508d66c16c722fa67abf8720839bd95f05
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-23 08:18:12 -0600

    All attribuets showing for orders index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/customers', to: 'customers#index'
   get '/customers/:id', to: 'customers#show'
   get '/orders', to: 'orders#index'
+  get '/orders/:id', to: 'orders#show'
 
 end
 
