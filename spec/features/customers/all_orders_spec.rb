f734aacd0e97352f4626a6d4c0738a040dbb5ae0
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-24 11:48:44 -0600

    refactored all orders for customer test

--- spec/features/customers/all_orders_spec.rb
+++ spec/features/customers/all_orders_spec.rb
@@ -1,49 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe 'the orders of a particular customer page' do
-  it 'displays the order quantity' do
-    customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
-    order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
+RSpec.describe 'the customers index page', type: :feature do 
+  describe "As a user" do
+    describe "When I visit /customers/id" do
+      it 'I can see the quantity of the order' do
+        customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
+        order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
 
-    visit "/customers/#{customer.id}/orders" 
+        visit "/customers/#{customer.id}/orders" 
 
-    expect(page).to have_content(order.quantity)
-  end
+        expect(page).to have_content(order.quantity)
+      end
 
-  it 'displays the order gift status' do
-    customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
-    order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
+      it 'I can see the gift status of the order' do
+        customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
+        order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
 
-    visit "/customers/#{customer.id}/orders" 
+        visit "/customers/#{customer.id}/orders" 
 
-    expect(page).to have_content(order.gift)
-  end
+        expect(page).to have_content(order.gift)
+      end
 
-  it 'displays the orders timestamps' do
-    customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
-    order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
+      it 'I can see the timstamps of the order' do
+        customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
+        order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
 
-    visit "/customers/#{customer.id}/orders" 
+        visit "/customers/#{customer.id}/orders" 
 
-    expect(page).to have_content(order.created_at)
-    expect(page).to have_content(order.updated_at)
-  end
+        expect(page).to have_content(order.created_at)
+        expect(page).to have_content(order.updated_at)
+      end
 
-  it 'displays the orders customer id' do
-    customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
-    order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
-    visit "/customers/#{customer.id}/orders" 
+      it 'I can see the customer ID of the order' do
+        customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
+        order = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
+        visit "/customers/#{customer.id}/orders" 
 
-    expect(page).to have_content(order.customer_id)
-  end
+        expect(page).to have_content(order.customer_id)
+      end
 
-  it 'displays all of the customers orders' do
-    customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
-    order1 = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
-    order2 = customer.orders.create!(quantity: 3, gift: true, order_type: "polly pockets")
-    visit "/customers/#{customer.id}/orders" 
+      it 'I can see all of the orders a customer made' do
+        customer = Customer.create!(first_name: "Alaina", last_name: "Kneiling" , in_usa: true, credit_score: 801 )
+        order1 = customer.orders.create!(quantity: 1, gift: true, order_type: "green shirt")
+        order2 = customer.orders.create!(quantity: 3, gift: true, order_type: "polly pockets")
+        visit "/customers/#{customer.id}/orders" 
 
-    expect(page).to have_content(order1.order_type)
-    expect(page).to have_content(order2.order_type)
+        expect(page).to have_content(order1.order_type)
+        expect(page).to have_content(order2.order_type)
+      end
+    end
   end
 end
