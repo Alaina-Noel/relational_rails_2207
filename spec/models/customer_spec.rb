848e2349f16daae74132a9469f22e26dbac0b119
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-24 15:19:40 -0600

    Add first lil test for my order model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,13 +1,5 @@
 require 'rails_helper'
 
-describe Customer, type: :model do
-  describe 'instance methods' do
-    describe '#name' do
-      it 'returns true when the name is Mia' do
-        task = Customer.create!(id: 1, first_name: 'Mia', last_name: 'Anderson', in_usa: true, created_at: nil, updated_at: nil)
-
-        expect(task.name?).to be(true)
-      end
-    end
-  end
+RSpec.describe Customer do
+  it {should have_many :orders}
 end
