3a2f91772ca6eaeb9069fd5cb0ded97993f29b24
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-24 08:08:04 -0600

    Tests for orders show page

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -1,37 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'the orders show page' do
-  it 'displays the customers first_name'do
-    order = Order.create!(customer_id: 1, quantity: 40, gift: false, order_type: "kitchen knife")
-    visit "/orders/#{order.id}"
+  it 'displays the order id number' do
+    customer = Customer.create!(first_name: "Robert", last_name: "Kneiling" , in_usa: true, credit_score: 754 )
+    order = customer.orders.create!(quantity: 1, gift: true, order_type: "green pants")
+    visit '/orders'
 
-    expect(page).to have_content(order.quantity)
-  end
+    expect(page).to have_content(order.id)
+end
 
-  xit 'displays the customers first_name'do
-    order = Order.create!(customer_id: 1, quantity: 40, gift: false, order_type: "kitchen knife")
+  it 'displays the order gift status'do
+    customer = Customer.create!(first_name: "Robert", last_name: "Robertson" , in_usa: false, credit_score: 754 )
+    order = customer.orders.create!(customer_id: 1, quantity: 40, gift: false, order_type: "kitchen knife")
     visit "/orders/#{order.id}"
 
     expect(page).to have_content(order.gift)
 end
 
-  xit 'displays the customers first_name'do
-    order = Order.create!(customer_id: 1, quantity: 400, gift: false, order_type: "kitchen set")
-    visit "/orders/#{order.id}"
-
-    expect(page).to have_content(order.order_type)
-  end
-
-  xit 'displays the customers id'do
-    order = Order.create!(customer_id: 1, quantity: 400, gift: false, order_type: "kitchen set")
+  it 'displays the customers id to whom the order belongs' do
+    customer = Customer.create!(first_name: "Sarah", last_name: "Robertson" , in_usa: false, credit_score: 755 )
+    order = customer.orders.create!(customer_id: 1, quantity: 400, gift: false, order_type: "kitchen set")
     visit "/orders/#{order.id}"
 
     expect(page).to have_content(order.customer_id)
   end
 
+  it 'displays the order gift status' do
+    customer = Customer.create!(first_name: "Sarah", last_name: "Robertson" , in_usa: false, credit_score: 755 )
+    order = customer.orders.create!(customer_id: 1, quantity: 400, gift: false, order_type: "kitchen set")
+    visit "/orders/#{order.id}"
 
-  xit 'displays the customers timestamps'do
-    order = Order.create!(customer_id: 1, quantity: 400, gift: false, order_type: "kitchen set")
+    expect(page).to have_content(order.gift)
+  end
+
+
+  it 'displays the order type' do
+    customer = Customer.create!(first_name: "Sarah", last_name: "Robertson" , in_usa: false, credit_score: 755 )
+    order = customer.orders.create!(customer_id: 1, quantity: 400, gift: false, order_type: "kitchen set")
+    visit "/orders/#{order.id}"
+
+    expect(page).to have_content(order.order_type)
+  end
+
+  it 'displays the order timestamps' do
+    customer = Customer.create!(first_name: "Sarah", last_name: "Robertson" , in_usa: false, credit_score: 755 )
+    order = customer.orders.create!(customer_id: 1, quantity: 400, gift: false, order_type: "kitchen set")
     visit "/orders/#{order.id}"
 
     expect(page).to have_content(order.created_at)
