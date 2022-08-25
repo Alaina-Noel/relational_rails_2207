43f0c133ba68dfb85005223d80cfa7670b06f5b0
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-25 08:11:42 -0600

    delete unnecesary method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,9 +8,5 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     @order_count = @customer.orders.count
   end
-
-  def all_orders
-    @orders_of_parent = Order.where(customer_id: params[:id])
-  end
   
 end
