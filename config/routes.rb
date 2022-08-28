19e26e165fa1ba7d1d257e07f41a19a6f380dd13
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-28 11:21:55 -0600

    Degbugged routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,23 @@ Rails.application.routes.draw do
 
   get '/customers', to: 'customers#index'
   get '/customers/new', to: 'customers#new'
+  post '/customers', to: 'customers#create'
   get '/customers/:id', to: 'customers#show'
-  get '/customers/:id/edit', to: 'customers#edit'
 
   get '/orders', to: 'orders#index'
   get '/orders/:id', to: 'orders#show'
+
+  get '/customers/:id/edit', to: 'customers#edit'
   get '/orders/:id/edit', to: 'orders#edit'
 
 
   get '/customers/:id/orders', to: 'customer_orders#index'
   get '/customers/:id/orders/new', to: 'customer_orders#new'
-  post '/customers', to: 'customers#create'
   post '/customers/:id/orders', to: 'customer_orders#create'
 
   patch '/customers/:id', to: 'customers#update'
   patch '/orders/:id', to: 'orders#update'
+
 
   delete '/customers/:id', to: 'customers#destroy'
   delete '/orders/:id', to: 'orders#destroy'
