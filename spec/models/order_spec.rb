49caf031840c94b4212508934faa5d5cd8aeade3
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-28 20:42:24 -0600

    passing test for find_orders_above method

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   it {should belong_to :customer}
+
+  describe 'instance methods' do
+    describe '#find_orders_above' do
+      it 'returns the orders above a certian quantity' do
+        customer1 = Customer.create(first_name: "Alaina", last_name: "Kneiling", in_usa: true, credit_score: 502)
+        order1 = customer1.orders.create!(quantity: 44, gift: true, order_type: "green pants")
+        order2 = customer1.orders.create!(quantity: 34, gift: true, order_type: "green shirt")
+        order3 = customer1.orders.create!(quantity: 24, gift: true, order_type: "green shoes")
+        order4 = customer1.orders.create!(quantity: 14, gift: true, order_type: "green polly pockets")
+
+        expect(Order.find_orders_above(34)).to eq([order1])
+      end
+    end
+  end
 end
